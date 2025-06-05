ce56446d1fc798f29351ff13f786bff062ac4cab
anyounghwa <e89cokr@naver.com>
2025-06-05 13:12:32 +0900

    layout.html viewport추가

--- src/main/java/com/example/tint/ProcessScheduler.java
+++ src/main/java/com/example/tint/ProcessScheduler.java
@@ -9,6 +9,7 @@ import com.example.tint.repository.ProcessRepository;
 import com.example.tint.service.JobOrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
